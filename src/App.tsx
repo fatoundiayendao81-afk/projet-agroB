ed13712712081b30d89ba936816923831e4f3649
AbdoulSalam407 <abdoulsalamd742@gmail.com>
2025-10-08 00:20:44 +0000

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -28,8 +27,11 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <p className="text-center text-gray-500 mt-4 text-lg font-semibold">
+        This is a Tailwind CSS styled paragraph.
+      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
